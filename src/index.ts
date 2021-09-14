910f7dec6ca922d70c08e511c25c6276f07a2e0b
hbybyyang <2451759073@qq.com>
2021-09-14 20:31:13 +0800

    debug

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export default async function 等待(条件: () => boolean, 超时时间: number
         var 计数器 = 0
         function f() {
             计数器++
-            if (计数器 * 轮询时间 >= 超时时间) throw '等待超时'
+            if (计数器 * 轮询时间 >= 超时时间) return rej('等待超时')
             if (条件()) return res(null)
             setTimeout(f, 轮询时间)
         }
