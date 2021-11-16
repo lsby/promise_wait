8e592946668bf05002a1eeaf24e0cdc7f42c7bde
hbybyyang <2451759073@qq.com>
2021-11-17 07:13:13 +0800

    增加睡眠函数, 不使用默认导出

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export default async function 等待(条件: () => boolean, 超时时间: number = 5000, 轮询时间: number = 100) {
+export async function 等待(条件: () => boolean, 超时时间: number = 5000, 轮询时间: number = 100) {
     if (轮询时间 == 0) 轮询时间 = 1
     return new Promise((res, rej) => {
         var 计数器 = 0
@@ -9,5 +9,12 @@ export default async function 等待(条件: () => boolean, 超时时间: number
             setTimeout(f, 轮询时间)
         }
         f()
+    })
+}
+export async function 睡眠(时间: number) {
+    return new Promise((res, rej) => {
+        setTimeout(function () {
+            res(null)
+        }, 时间)
     })
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,4 @@
-import 等待 from '../dist/'
+import { 等待, 睡眠 } from '../dist/'
 
 describe('基本测试', function () {
     it('测试1', async function () {
@@ -7,5 +7,10 @@ describe('基本测试', function () {
             a = 2
         }, 1000)
         await 等待(() => a == 2)
+    })
+    it('测试2', async function () {
+        var a = 1
+        await 睡眠(1000)
+        a = 2
     })
 })
